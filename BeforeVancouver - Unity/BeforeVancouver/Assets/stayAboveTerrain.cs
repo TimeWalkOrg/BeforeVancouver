fbf2377ab190a310c6864c5d061ef0109413bfb9
tedbarnett <mail@tedbarnett.com>
2018-08-03 21:50:19 -0700

    Terrain and tree improvements

---
+++ BeforeVancouver - Unity/BeforeVancouver/Assets/stayAboveTerrain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stayAboveTerrain : MonoBehaviour
+{
+    public float yAddition;
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.y = Terrain.activeTerrain.SampleHeight(transform.position) + yAddition;
+        transform.position = pos;
+    }
+}
+
+
